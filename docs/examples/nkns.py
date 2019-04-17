ba2bab45c3c28e59e2cc0a61487185530519bc3d
G. D. McBain <gdmcbain@protonmail.com>
2019-04-17 15:54:54 +1000

    spilu gmres for steady Navier-Stokes Newton iteration #169

---
+++ docs/examples/nkns.py
@@ -0,0 +1,53 @@
+from ns import *
+
+from scipy.sparse.linalg import splu, spilu, gmres, LinearOperator
+
+
+def creeping(step):
+    """return the solution for zero Reynolds number"""
+    uvp = step.make_vector()
+    uvp[step.I] = step.lu0.solve(
+        condense(step.S, np.zeros_like(uvp), uvp, step.I)[1])
+    return uvp
+
+
+def jacobian_solver(step,
+                    uvp: np.ndarray,
+                    reynolds: float,
+                    rhs: np.ndarray) -> np.ndarray:
+    duvp = step.make_vector() - uvp
+    u = step.basis['u'].interpolate(step.split(uvp)[0])
+    A = (step.S
+         + reynolds * block_diag([asm(acceleration_jacobian,
+                                      step.basis['u'], w=u),
+                                  csr_matrix((step.basis['p'].N,)*2)]))
+    A1 = condense(A, I=step.I).tocsc()
+    ilu = spilu(A1, 1e-5 * 1., 1e1 * 1.)
+    _, rhs1 = condense(A, rhs, duvp, I=step.I)
+    duvp[step.I], info = gmres(A1, rhs1, step.lu0.solve(rhs1), 1e-12,
+                               M=LinearOperator(ilu.L.shape, ilu.solve))
+
+    if info:
+        raise RuntimeError(info)
+    else:
+        return duvp
+
+
+bfs.lu0 = splu(condense(bfs.S, I=bfs.I).T)
+bfs.__class__.creeping = creeping
+bfs.__class__.jacobian_solver = jacobian_solver
+
+
+if __name__ == '__main__':
+
+    from os.path import splitext
+    from sys import argv
+
+    try:
+        natural(bfs, bfs.creeping(), 0.,
+                partial(callback, name=splitext(argv[0])[0]),
+                lambda_stepsize0=50.,
+                lambda_stepsize_max=150.,
+                newton_tol=1e-9)
+    except RangeException:
+        print(f'Reynolds number sweep complete: {re}.')
