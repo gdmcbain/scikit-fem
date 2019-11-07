577c68a57a67f95607a21e26b9289aa29c589c1f
G. D. McBain <gdmcbain@protonmail.com>
2019-11-07 16:41:11 +1100

    plot pressure

--- docs/examples/ex_vc.py
+++ docs/examples/ex_vc.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from skfem import *
 
+name = splitext(argv[0])[0]
+
 mesh = MeshTri.init_symmetric()
 mesh.refine(3)
 
@@ -12,14 +14,14 @@ element = {'u': ElementTriP2(), 'p': ElementTriP1()}
 basis = {v: InteriorBasis(mesh, e, intorder=3) for v, e in element.items()}
 
 p = basis['p'].doflocs[0]       # p (x, y) = x
-print(p)
+ax = mesh.plot(p)
+ax.get_figure().savefig(f'{name}-pressure.png')
 
 u, v = (derivative(p, basis['p'], basis['u'], i) for i in range(2))
 
-name = splitext(argv[0])[0]
 ax = mesh.draw()
 ax.quiver(*mesh.p, u[basis['u'].nodal_dofs], v[basis['u'].nodal_dofs])
-ax.get_figure().savefig(f'{name}.png')
+ax.get_figure().savefig(f'{name}-velocity.png')
 
 
 
